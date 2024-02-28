6fbe7389ca49b91204d1df045c087a8aab193901
ByloTonix <bylotonix@gmail.com>
2024-02-28 12:32:42 +0300

    setWeights

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,8 @@
-#include <iostream>
-#include <vector>
-#include <string>
 #include "titanic.h"
 
 const std::string PATH = "../data/titanic.csv";
 
 int main() {
-
-   
     stringMatrix matrix = readCsv(PATH);
     for (int i: {CSV_FIELDS::Age, CSV_FIELDS::Sex, CSV_FIELDS::Pclass})
         fillNan(matrix, i);
@@ -17,6 +12,7 @@ int main() {
     writeCsv("output.csv", matrix);
     // std::cout << defineRate(0, 1, 1);
     selectSurvivors(matrix, 10, 10);
+    getObesity("../data/obesity.csv");
     return 0;
 }
 
